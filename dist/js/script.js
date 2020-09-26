4bd7fc2faa2aeed36541be37232e9a88013b2f43
Mykhail <gelios238@gmail.com>
2020-09-26 19:19:53 +0300

    added slider

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,21 +1,96 @@
-$(document).ready(function(){
-    $('.header__slider').slick({
-        arrows: false
-    });
-  });
+'Use strict';
+window.addEventListener('DOMContentLoaded', () => {
 
-$(document).ready(function(){
-    $('.solution__slider_wrapper').slick({
-        variableWidth: true,
-      
-        prevArrow: '<button type="button" class="slick-prev"><img src="../img/solutions/arrow_for_slider_left.svg" alt=""></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/solutions/arrow_for_slider_right.svg" alt=""></button>'
+//slider 
+
+    function generalSlider(num) {
+
+        const   
+            slider = document.querySelectorAll('.solution__slider'),
+            windowSlider = slider[num].querySelector('.solution__slider_wrapper'),
+            sliderField = slider[num].querySelector('.solution__slider_inner'),
+            slides = slider[num].querySelectorAll('.solution__slider_img'),
+            widthSlide = deleteNotDigital(window.getComputedStyle(windowSlider).width),
+            nextSlide = slider[num].querySelector('.solution__slider_next'),
+            prevSlide = slider[num].querySelector('.solution__slider_prev'),
+            copyLastSlide = slides[slides.length - 1].cloneNode(true),
+            copyFirstSlide = slides[0].cloneNode(true);
+            
+        let index = 0,
+            positInit = -widthSlide,
+            allowShift = true;
+
+        sliderField.style.left = `${positInit}px`;
+        sliderField.style.width = `${widthSlide * slides.length}px`;
+        windowSlider.style.overflow = 'hidden';
+        
+        sliderField.append(copyFirstSlide);
+        sliderField.prepend(copyLastSlide);
+
+        function deleteNotDigital(str){
+            return Math.round(+str.replace(/[A-Za-z]/g, ''));
+        }
+            
+        function shiftSlide(dir) {
+            if(allowShift){
+                sliderField.classList.add('shifting');
+
+                positInit = sliderField.offsetLeft;
+
+                if(dir == 1){
+                    ++index;
+                    sliderField.style.left = `${positInit - widthSlide}px`;
+                }else if(dir == -1){
+                    --index;
+                    sliderField.style.left = `${positInit + widthSlide}px`;
+                }
+            }
+            allowShift = false;
+        }
+
+        function checkIndex(){
+            sliderField.classList.remove('shifting');
+            
+            if(index == slides.length){
+                index = 0;
+                positInit = -widthSlide;
+                sliderField.style.left = `${positInit}px`;
+            }else if(index == -1){
+                index = slides.length - 1;
+                positInit = -widthSlide;
+                sliderField.style.left = `${positInit * slides.length}px`;
+            }
+            allowShift = true;
+        }
+
+        nextSlide.addEventListener('click', () => shiftSlide(1));
+        prevSlide.addEventListener('click', () => shiftSlide(-1));
+        sliderField.addEventListener('transitionend', checkIndex);
+
+    }
+    generalSlider(0);
+
+    //tabs
+
+    const 
+        tabsBtn = document.querySelectorAll('.solution__tab_nav li'),
+        tabsItem = document.querySelectorAll('.solution__slider');
+
+    tabsBtn.forEach((btn, index) => {
+        btn.setAttribute('data-tabs', index);
+        btn.addEventListener('click', toggleTabs);
     });
 
-    $('ul.solution__tab_nav').on('click', 'li:not(.active)', function() {
-        $(this)
-          .addClass('active').siblings().removeClass('active')
-          .closest('div.solution__tab').find('div.solution__slider').removeClass('solution__slider-active').eq($(this).index()).addClass('solution__slider-active');
-      });
-  });
+    function toggleTabs (e) {
+        tabsBtn.forEach(btn => btn.classList.remove("active"));
+        e.target.classList.add('active');
+
+        const valueAtr = e.target.getAttribute('data-tabs');
+        tabsItem.forEach(item => item.classList.remove('solution__slider-active'));
+        tabsItem[valueAtr].classList.add('solution__slider-active');
+
+        generalSlider(valueAtr);
+    }
+});
+
 
